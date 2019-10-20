2c196d4af39892c3f5baad2bb4aa63cf975cd27a
Eugene Pyrlog <evgeniipyrlog@mail.ru>
2019-10-20 18:16:30 +0300

    Add rest_framework login api

--- DjangoRESTAuth/DjangoRESTAuth/urls.py
+++ DjangoRESTAuth/DjangoRESTAuth/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/auth/', include('jwt_auth.urls')),
+    path('api/auth/', include('rest_framework.urls')),
+    path('api/jwt-auth/', include('jwt_auth.urls')),
     path('api/users/', include('users.urls')),
 ]
